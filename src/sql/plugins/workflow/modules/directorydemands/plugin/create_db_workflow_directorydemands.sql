6bac382f78a47626758da8e8431324933cfb928d
AlexandreClose <alexandre.close@gmail.com>
2017-10-12 14:09:02 +0200

    WDIRDEM-1 : Fix init sql script with directory_record_unit_assignment as table name

--- src/sql/plugins/workflow/modules/directorydemands/plugin/create_db_workflow_directorydemands.sql
+++ src/sql/plugins/workflow/modules/directorydemands/plugin/create_db_workflow_directorydemands.sql
@@ -2,7 +2,7 @@
 /* Table structure for table workflow_directorydemands_record_assignment   */
 /*==================================================================*/
 DROP TABLE IF EXISTS directory_record_unit_assignment;
-CREATE TABLE directory_record_unitassignment (
+CREATE TABLE directory_record_unit_assignment (
   id int(11) NOT NULL DEFAULT '0',
   id_record int(11) NOT NULL DEFAULT '0',
   id_assignor_unit int(11) NOT NULL DEFAULT '0',
@@ -16,7 +16,7 @@ CREATE TABLE directory_record_unitassignment (
 /*==================================================================*/
 /* Indexes creation for module workflow_directorydemands */
 /*===================================================================*/
- CREATE INDEX index_directory_record_unitassignment_id_record ON directory_record_unit_assignment (id_record);
- CREATE INDEX index_directory_record_unitassignment_id_assigned_unit ON directory_record_unit_assignment (id_assigned_unit,assignment_date);
- CREATE INDEX index_directory_record_unitassignment_id_assignor_unit ON directory_record_unit_assignment (id_assignor_unit,assignment_date);
+ CREATE INDEX index_directory_record_unit_assignment_id_record ON directory_record_unit_assignment (id_record);
+ CREATE INDEX index_directory_record_unit_assignment_id_assigned_unit ON directory_record_unit_assignment (id_assigned_unit,assignment_date);
+ CREATE INDEX index_directory_record_unit_assignment_id_assignor_unit ON directory_record_unit_assignment (id_assignor_unit,assignment_date);
 
