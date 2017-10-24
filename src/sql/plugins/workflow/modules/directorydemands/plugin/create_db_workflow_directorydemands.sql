bb1956b877cdbd21b7223e7e231663b2cdc10752
seboo <25958061+seboo@users.noreply.github.com>
2017-10-24 15:51:18 +0200

    WDIRDEM-1 : add Drop table if exists

--- src/sql/plugins/workflow/modules/directorydemands/plugin/create_db_workflow_directorydemands.sql
+++ src/sql/plugins/workflow/modules/directorydemands/plugin/create_db_workflow_directorydemands.sql
@@ -45,6 +45,7 @@ CREATE TABLE workflow_task_directorydemands_assign_to_unit_cf (
 /*==================================================================*/
 /* Create table for unit tree code attribute */
 /*===================================================================*/
+DROP TABLE IF EXISTS unittree_unit_code ;
 CREATE TABLE unittree_unit_code (
 	id_unit INT DEFAULT 0 NOT NULL,
 	unit_code VARCHAR(255) DEFAULT '' NOT NULL,
